4676027edcb4419167eab82de017de56fc08150d
Jaqa97 <joaquinonesa@gmail.com>
2022-12-21 17:22:16 +0000

    Final commit

--- src/js/component/traffic.jsx
+++ src/js/component/traffic.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 import { useState } from "react";
 
-
 export function Traffic() {
-    const[luz, setLuz] = useState("")
-  return (<>
-
+  const [luz, setLuz] = useState("");
+  return (
+    <>
       <h1>Traffic</h1>
       <div className="border rounded-4 container d-flex justify-content flex-column">
-
-        <button className={luz==="red rounded-circle m-2"? "red rounded-circle m-2 encendido": "red rounded-circle m-2"} onClick={()=>{setLuz("red rounded-circle m-2")}}></button>
-        <button className="yellow rounded-circle m-2"></button>
-        <button className="green rounded-circle m-2"></button>
+        <button
+          className={luz === "red" ? "red encendido" : "red"}
+          onClick={()=>{
+            setLuz("red");
+      
+          }}
+        ></button>
+        <button className={luz === "yellow" ? "yellow encendido" : "yellow"}
+          onClick={() => {
+            setLuz("yellow");
+          }}></button>
+        <button className={luz === "green" ? "green encendido" : "green"}
+          onClick={() => {
+            setLuz("green");
+          }}></button>
       </div>
     </>
-
   );
 }
